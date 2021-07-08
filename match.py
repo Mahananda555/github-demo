6dec974906bf8c37cbf14a2150eea6f3c2756224
Mahananda555 <71594136+Mahananda555@users.noreply.github.com>
2021-07-08 14:28:51 +0530

    initial commit  file math.py

---
+++ match.py
@@ -0,0 +1,14 @@
+# implement
+def add(x,y):
+ pass
+def subtract(x,y):
+    pass
+	return x-y   # sub implen
+def multiply(x,y):
+ pass
+ return x*y
+def divide(x,y):
+    pass
+def square(x)
+pass 
+
