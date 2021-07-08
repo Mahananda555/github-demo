b5366d55ea6a897e498cc406407053ad8bd0992e
Mahananda555 <mahanandagurude555@gmail.com>
2021-07-08 14:37:39 +0530

    add all info

--- match.py
+++ match.py
@@ -1,12 +1,15 @@
-# implement
+# add implement
 def add(x,y):
  pass
+# sub implementation
 def subtract(x,y):
     pass
-	return x-y   # sub implen
+	return x-y   
+# mul implementation
 def multiply(x,y):
  pass
  return x*y
+ # div implen
 def divide(x,y):
     pass
 def square(x)
